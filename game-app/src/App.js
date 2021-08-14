78bb484ff886900f43c54e0b872cdce35f58dc2c
Halie Chmura <halie.chmura@gmail.com>
2021-08-13 20:58:30 -0400

    starter code

---
+++ game-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
